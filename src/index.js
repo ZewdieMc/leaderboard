533a908eb58386e255d6c7e9088b305184679afa
ZewdieMc <zewdiehabtie26@gmail.com>
2023-03-09 00:52:04 +0300

    Fix lighthouse error

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './index.css';
 import scores from './modules/score.js';
 
-document.querySelector('#scores').innerHTML = scores
-  .map((score) => `<li>${score.name}: ${score.score}</li>`)
-  .join('');
+window.onload = () => {
+  document.querySelector('#scores').innerHTML = scores
+    .map((score) => `<li>${score.name}: ${score.score}</li>`)
+    .join('');
+};
