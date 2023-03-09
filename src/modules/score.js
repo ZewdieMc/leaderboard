c11125d8c258f7b9ff1beff5ca6ef2e1bcd43c0a
ZewdieMc <zewdiehabtie26@gmail.com>
2023-03-09 11:01:51 +0300

    Modify score values

--- src/modules/score.js
+++ src/modules/score.js
@@ -3,7 +3,7 @@ export default [
   { name: 'name', score: 80 },
   { name: 'name', score: 91 },
   { name: 'name', score: 70 },
+  { name: 'name', score: 65 },
   { name: 'name', score: 70 },
-  { name: 'name', score: 70 },
-  { name: 'name', score: 70 },
+  { name: 'name', score: 40 },
 ];
