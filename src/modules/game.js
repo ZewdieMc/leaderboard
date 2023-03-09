b606b498fb2a62d70b886bf39b856fe6fa36754e
ZewdieMc <zewdiehabtie26@gmail.com>
2023-03-09 17:55:10 +0300

    Implement getScores method

--- src/modules/game.js
+++ src/modules/game.js
@@ -1,9 +1,10 @@
 const myGame = { name: 'my new game' };
-
+const gameID = 'u4sSeeKHqAktLAVLfPrr';
 const baseURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api';
 const gameEndpoint = '/games/';
+const scoresEndPoint = '/scores/';
 
-export default class Game {
+class Game {
   newGame = async () => {
     const response = await fetch(`${baseURL}${gameEndpoint}`, {
       method: 'POST',
@@ -14,4 +15,20 @@ export default class Game {
     });
     return response.json();
   }
+
+  getScores = async () => {
+    try {
+      const response = await fetch(baseURL + gameEndpoint + gameID + scoresEndPoint,
+        {
+          method: 'GET',
+          headers: { 'Content-type': 'application/json; charset=UTF-8' },
+        });
+      return response.json();
+    } catch (err) {
+      return err;
+    }
+  };
 }
+
+const game = new Game();
+export default game;
