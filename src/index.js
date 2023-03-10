1af0b8efd12fcb0ffec8ad7ce7e38a4151a0d493
ZewdieMc <zewdiehabtie26@gmail.com>
2023-03-10 17:22:13 +0300

    Add alert on saving score.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './index.css';
+import './index.scss';
 import game from './modules/game.js';
 
 window.onload = () => game.renderScores();
@@ -9,9 +9,9 @@ scoreForm.onsubmit = (e) => {
   const result = game.saveScore();
   if (result) {
     result.then((res) => {
-      document.querySelector('#information').textContent = res.result;
-      document.querySelector('#information').classList.remove('hidden');
-      setTimeout(() => document.querySelector('#information').classList.add('hidden'), 3000);
+      document.querySelector('#info').textContent = res.result;
+      document.querySelector('#info').classList.remove('hidden');
+      setTimeout(() => document.querySelector('#info').classList.add('hidden'), 3000);
     });
   }
 };
